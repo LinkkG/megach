4b7bba7cb6c7e212ab363011ab8b0add24d379ac
Julio <Juliocgc32@gmail.com>
2018-06-22 19:43:55 -0600

    Simplificado el uso de algunos comandos
    MultiPrefijo

--- Ejemplobot.py
+++ Ejemplobot.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 Archivo de prueba para mi versión del ch
 Incluirá las opciones básicas e iré añadiendo más poco a poco
+Versión 1.5.1
 """
 # Módulos básicos
-# import os
+import math
+import os
 import socket
 import sys
 import time
-# import urllib.request as urlreq
-# import urllib.parse as urlparse
-# import queue
+import urllib.request as urlreq
+import urllib.parse as urlparse
 
 #  Módulos del bot
 import megach
@@ -22,11 +24,13 @@ import megach
 # TODO Interfaz gráfica
 # Dueños, Solo minúsculas
 owners = ['megamaster12', 'linkkg', 'milton797']
-debug = True  # Printear cosas extra para depuración
+# Depuración
+
+debug = False  # Printear cosas extra para depuración
 debugchats = ['pythonrpg', 'PM']  # Conexiones donde se printeará lo recibido
 # ignored commands onRaw
-ignored_rcmd_ = ["b", "i", "u"] + ['']
-prefijos = '|'.split()  # Uno o más prefijos separados por espacio
+ignored_rcmd_ = "i u OK".split() + ['']
+prefijos = '| %'.split()  # Uno o más prefijos separados por espacio
 
 
 class Mibot(megach.Gestor):
@@ -41,15 +45,11 @@ class Mibot(megach.Gestor):
     def onInit(self):
         """Espacio para ejecutar acciones de carga de información u otras cosas"""
         print("Inicio del BOT")
-        self.setNameColor(
-                "FF0000")  # el color del nombre de usuario(el mio es rojo), puedes cambiar los 6 numeros para
-        # cambiar el
-        #  color
+        self.setNameColor("FF0000")  # el color del nombre de usuario en hexadecimal
         self.setFontColor("00FF33")  # el color de la letra
         self.setFontFace("Typewriter")  # el tipo de letra, usas los tipos de letra que hay en chatango
         self.setFontSize(12)  # tamaño de la letra, supuestamente el maximo es 14 sin premium
         self.enableBg()  # por si le pones bg al bot, esto se lo activa
-        self.enableRecording()  # ni idea de pa que es esto en chatango, pero lo activa si está disponible xd
     
     def onConnect(self, room):
         """Al haberse conectado a una sala de chat"""
@@ -91,24 +91,42 @@ class Mibot(megach.Gestor):
                 # Aqui comienzan los comandos, puedes poner los que quieras mientras esten dentro
                 ################################################################
                 if cmd == "all":
-                    usuarios = sorted([x.showname for x in room._getUserlist(1)])
+                    usuarios = sorted(room.shownames)
                     usuarios = '{} y {}'.format(', '.join(usuarios[:-2]), usuarios[-1])
                     room.message("Lista completa de los usuarios: " + usuarios)
                     # usuarios=sorted([(x.showname + ( ('(%s)' % len(x._getSessionIds(room))) if len(
                     # x._getSessionIds(room)) > 1 else '')) for x in
                     #                              room._getUserlist(1)])
+                elif cmd in ['conecta', 'join'] and user.name in owners:
+                    if self.joinRoom(args):
+                        room.message("Conectado ♪")
+                    else:
+                        room.message("No puedo ir...")
+
+                elif cmd == "desconecta" and user.name in owners:
+                    if args.lower() in self._rooms:
+                        self.leaveRoom(args)
+                        room.message("Desconectado")
+                    else:
+                        room.message("No estoy conectado a " + args)
+
+
+                elif cmd in ["donde", "where"]:
+                    salas = ["{} ({})".format(x.name, x.userCount) for x in self.rooms]
+                    room.message("Estoy en: " + ', '.join(salas))
+                
                 elif cmd == "baila":
                     room.message("o(^^o) ---- ^(oo)^ ---- (o^^)o")
-    
+
                 elif cmd in ["say", "write"]:
                     room.message(args)
-    
+
                 ################################################################
                 # Sección para desarrolladores
                 ################################################################
                 if user.name in owners:
                     if cmd in ['eval', 'ev'] and args:
-                        args = message._body.split(' ', 1)[1]
+                        args = message.fullbody.split(' ', 1)[1]
                         try:
                             room.message(str(eval(args)))
                         except Exception as err:
@@ -120,13 +138,6 @@ class Mibot(megach.Gestor):
                         room.message(str(exec(args)))
         except Exception as e:
             room.message(str(e))
-
-    def onPing(self, room):
-        """
-
-        @param room:
-        """
-        print("[{}][{:^10.10}] Ping enviado".format(time.strftime("%I:%M:%S %p"), room.name))
     
     def onPMConnect(self, pm):
         """
@@ -135,18 +146,19 @@ class Mibot(megach.Gestor):
         @return: None
         """
         print("[{}] Conectado al PM como {}".format(time.strftime('%I:%M:%S %p'), pm.currentname))
-    
-    def onPong(self, room, pong):
+
+    def onPMMessage(self, pm, user, message):
         """
-        Al recibir un pong de una sala
-        @param room: La sala en la que se recibió el pong
-        @param pong: Posibles datos del pong
+        Al recibir un mensaje en el PM
+        @param pm: El PM
+        @param user: El usuario que manda el mensaje
+        @param message: El mensaje recibido
         """
-        print("[{}][{:_^10.10}] Pong recibido".format(time.strftime("%I:%M:%S %p"), room.name))
+        print("[{}][{:_^10.10}][{}]:{}".format(time.strftime("%I:%M:%S %p"), pm.name, user.name, message))
     
     def onRaw(self, room, raw):
         """Al recibir un comando de chatango"""
-        if raw.split(':')[0] not in ignored_rcmd_:
+        if debug and raw.split(':')[0] not in ignored_rcmd_:
             if room.name in debugchats:
                 print('[{0}][{1:_^10.10}]{2}'.format(time.strftime("%I:%M:%S %p"), room.name, raw), file = sys.stderr)
 
