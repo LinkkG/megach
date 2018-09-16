b50724824843939a560baaf289d0afba4fe148d1
TheClonerx <theclonerx@outlook.com>
2018-09-15 23:03:43 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+url = "https://github.com/LinkkG/megach/"
+
+setup(
+    name="megach",
+    author="Link (LinkkG) Arias Martínez",
+    email="supermegamaster32@gmail.com",
+    description="A library to connect to multiple Chatango rooms.",
+    url=url,
+    download_url=url + "/archive/master.zip",
+    license="GNU General Public License v3.0",
+    py_modules=["megach"]
+)
