13610f65562445906764d09b3d8a8f61db668576
Julio <Juliocgc32@gmail.com>
2018-09-16 20:05:56 -0600

    Test change

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Setup file"""
 from distutils.core import setup
+import megach
 
 url = "https://github.com/LinkkG/megach/"
 
 setup(
-    name="megach",
-    author="Link (LinkkG) Arias Martínez",
-    email="supermegamaster32@gmail.com",
-    description="A library to connect to multiple Chatango rooms.",
-    url=url,
-    download_url=url + "/archive/master.zip",
-    license="GNU General Public License v3.0",
-    py_modules=["megach"]
-)
+        name = "megach",
+        author = "Link (LinkkG) Arias Martínez",
+        author_email = "supermegamaster32@gmail.com",
+        description = "A library to connect to multiple Chatango rooms.",
+        url = url,
+        download_url = url + "/archive/master.zip",
+        license = "GNU General Public License v3.0",
+        py_modules = ["megach"],
+        version = megach.version,
+        )
